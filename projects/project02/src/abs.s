10ea0e6ac014aeacc808328f4e68333e6a4d7c60
jiang <1813887669@qq.com>
2025-11-10 21:00:37 +0800

    finish abs.s

--- projects/project02/src/abs.s
+++ projects/project02/src/abs.s
@@ -9,10 +9,21 @@
 #   None
 # =================================================================
 abs:
-    # Prologue
+    # 读取整数
+    lw t0, 0(a0)      
+    # 如果是负数，跳转  
+    blt t0, x0, make_pos 
+    # 否则跳过修改
+    j end               
 
-    # PASTE HERE
+make_pos:
+    # 取反
+    neg t0, t0         
 
-    # Epilogue
+end:
+    # 写回结果
+    sw t0, 0(a0)     
+    # 返回   
+    jr ra              
 
-    jr ra
+
